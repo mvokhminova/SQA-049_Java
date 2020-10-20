efb16563e1bae734ade60f9da9062f27c12ca5b1
mvokhminova <mvokhminova@luxoft.com>
2020-10-20 22:31:21 +0300

    test

--- Calc2/src/com/company/Main.java
+++ Calc2/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,38 +10,40 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         while (!"q".equals(userInput)) {
-            System.out.println("Enter number one:");
-            int firstNumber = scanner.nextInt();
-            System.out.println("Enter number two:");
-            int secondNumber = scanner.nextInt();
-            System.out.println("Enter operation:");
-            userInput = scanner.next();
-
+            System.out.println("Enter expression:");
             double result;
+            userInput = scanner.nextLine();
+            if(!Pattern.matches("q",userInput.trim())) {
 
-            switch (userInput){
-                case "+":
-                    result = plus(firstNumber, secondNumber);
-                    break;
-                case "-":
-                    result = minus(firstNumber, secondNumber);
-                    break;
-                case "*":
-                    result = ymn(firstNumber, secondNumber);
-                    break;
-                case "/":
-                    if(secondNumber == 0){
-                        continue;
-                    }
-                    result = del(firstNumber, secondNumber);
-                    break;
-                case "!":
-                    result = fact(firstNumber);
-                    break;
-                default:
-                    result = 0;
-                    break;
+                String[] expr = userInput.split("\\s*|(\\+|\\-|\\*|\\/)");
+                int firstNumber = Integer.parseInt(expr[0]);
+                int secondNumber = Integer.parseInt(expr[2]);
+                userInput = expr[1];
+                switch (userInput) {
+                    case "+":
+                        result = plus(firstNumber, secondNumber);
+                        break;
+                    case "-":
+                        result = minus(firstNumber, secondNumber);
+                        break;
+                    case "*":
+                        result = ymn(firstNumber, secondNumber);
+                        break;
+                    case "/":
+                        if (secondNumber == 0) {
+                            continue;
+                        }
+                        result = del(firstNumber, secondNumber);
+                        break;
+                    case "!":
+                        result = fact(firstNumber);
+                        break;
+                    default:
+                        result = 0;
+                        break;
+                }
             }
+            else {break;}
             System.out.println(result);
         }
         scanner.close();
